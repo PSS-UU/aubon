e71604366b752726b1c25b65ddc0f5c4a1172bb3
Daniel Fehrm <daniel@fehrm.se>
2022-02-10 18:26:23 +0100

    replaced eslint airbnb for typescript airbnb

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -4,19 +4,26 @@ module.exports = {
         es2021: true
     },
     extends: [
-        'airbnb'
+        'airbnb-base',
+        'airbnb-typescript/base'
     ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaVersion: 'latest',
-        sourceType: 'module'
+        sourceType: 'module',
+        project: './tsconfig.json'
     },
     plugins: [
         '@typescript-eslint'
     ],
     rules: {
-        indent: ['error', 4],
-        'comma-dangle': ['error', 'never'],
+        // If a rule needs to be added or changed, start by looking on
+        // https://typescript-eslint.io/rules/ and see if the rule exists there.
+        // Otherwise use the normal eslint rules https://eslint.org/docs/rules/
+        indent: 'off',
+        '@typescript-eslint/indent': ['error', 4],
+        'comma-dangle': 'off',
+        '@typescript-eslint/comma-dangle': ['error', 'never'],
         'no-console': 'off'
     }
 };
