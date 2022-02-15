26fdc6dc68f91cd60f0d322c531d04c18f782ad7
Sofia Johansson <sofia.johansson@igimle.se>
2022-02-15 16:51:42 +0100

    map view

--- app/aubon_app/lib/main.dart
+++ app/aubon_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,17 +8,29 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+  static const token =
+      "pk.eyJ1IjoiYWxiaW5hbnR0aSIsImEiOiJja2p1ZHEwcGU4aWRnMnlsZ3Rsbm50Yzc1In0.CrAYg00ejOQY8mz7KnO39w";
+  static const mapUrl =
+      "https://api.mapbox.com/styles/v1/albinantti/ckzh3jx4r009q14l8eb32614u/tiles/{z}/{x}/{y}?access_token=" +
+          token;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Aubon',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: FlutterMap(
+          options: MapOptions(center: LatLng(59.86, 17.63), minZoom: 10.0),
+          layers: [
+            TileLayerOptions(urlTemplate: mapUrl,
+                //"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                subdomains: ['a', 'b', 'c'])
+          ],
+          //body: const Center(
+          //child: Text('Hello World'),
         ),
       ),
     );
