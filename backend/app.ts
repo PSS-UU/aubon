50ba64470742c4179c89f30af0f17bd80b71cfc5
Daniel Fehrm <daniel@fehrm.se>
2022-02-16 14:18:28 +0100

    Added post route for sending report
    Co-authored-by: Arvid Sandin <arvidsandin@users.noreply.github.com>

--- backend/app.ts
+++ backend/app.ts
@@ -1,13 +1,26 @@
 import 'dotenv/config';
 import express from 'express';
+import bodyParser from 'body-parser';
 import { connect as db_connect } from './database';
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 db_connect().then(() => {
     app.get('/', (req, res) => {
         res.send('Hello World!');
+    });
+
+    app.post('/send-report', (req, res) => {
+        // TODO: Add pictures
+        const long = req.body.longitude;
+        const lat = req.body.latitude;
+        const rating = req.body.auroraRating;
+        const timestamp = Date.now();
+        res.send([long, lat, rating, timestamp].join(', '));
     });
 
     app.listen(port, () => {
