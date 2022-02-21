c16f02982ca87034a2aca12dd20c75cccbef6588
Daniel Fehrm <daniel@fehrm.se>
2022-02-21 12:15:58 +0100

    Send reports API (#43)
    * Added post route for sending report
    
    Co-authored-by: Arvid Sandin <arvidsandin@users.noreply.github.com>
    
    * Store reports in database
    
    * Added error handling to send report
    
    Co-authored-by: Arvid Sandin <arvidsandin@users.noreply.github.com>
    
    * added lat and long validation
    
    Co-authored-by: Arvid Sandin <arvidsandin@users.noreply.github.com>
    
    * Add UserID
    
    Co-authored-by: Arvid Sandin <arvidsandin@users.noreply.github.com>
    Co-authored-by: Arvid Sandin <36199701+arvidsandin@users.noreply.github.com>

--- backend/app.ts
+++ backend/app.ts
@@ -1,13 +1,44 @@
 import 'dotenv/config';
 import express from 'express';
-import { connect as db_connect } from './database';
+import bodyParser from 'body-parser';
+import { connect as db_connect, insertReport } from './database';
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 db_connect().then(() => {
     app.get('/', (req, res) => {
         res.send('Hello World!');
+    });
+
+    app.post('/send-report', (req, res) => {
+        // TODO: Add pictures
+        const long = req.body.longitude;
+        const lat = req.body.latitude;
+        const rating = req.body.auroraRating;
+        const ID = req.body.userID;
+
+        if (rating < 0 || rating > 5) {
+            res.status(400);
+            res.send('Rating must be an integer between 0 and 5');
+            return;
+        }
+
+        if (long < -180 || long > 180 || lat < -90 || lat > 90) {
+            res.status(400);
+            res.send('Invalid coordinates');
+            return;
+        }
+
+        insertReport(long, lat, rating, ID).then((success: boolean) => {
+            if (!success) {
+                res.status(500);
+            }
+            res.send(success);
+        });
     });
 
     app.listen(port, () => {
