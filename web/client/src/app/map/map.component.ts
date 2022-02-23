ed359bfe1386f80cdabc55542faa3de940e434ca
Albin Antti <albin_antti@hotmail.com>
2022-02-23 16:47:50 +0100

    Implement displaying reports

--- web/client/src/app/map/map.component.ts
+++ web/client/src/app/map/map.component.ts
@@ -1,6 +1,6 @@
 import { Component, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
-import { interval, reduce, Timestamp } from 'rxjs';
+import { interval } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 
@@ -8,6 +8,8 @@ type Report = {
     user_id: number;
     rating: 0 | 1 | 2 | 3 | 4 | 5 ;
     time: string;
+    longitude: number;
+    latitude: number;
   };
 
 type Coordinate = {
@@ -15,10 +17,20 @@ type Coordinate = {
     long: number;
 };
 
-let uppsalaCentrum: Coordinate = {
+let centerCoords: Coordinate = {
     lat: 59.8585610,
     long: 17.6389242,
 }
+
+//TODO: Replace this list with an api call
+let reportList: Report[] = [
+    {user_id: 1, rating: 0, time: "just now", longitude: 17.61, latitude: 59.861},
+    {user_id: 2, rating: 1, time: "just now", longitude: 17.62, latitude: 59.862},
+    {user_id: 3, rating: 2, time: "just now", longitude: 17.63, latitude: 59.863},
+    {user_id: 4, rating: 3, time: "yesterday", longitude: 17.65, latitude: 59.84},
+    {user_id: 5, rating: 4, time: "three hours ago", longitude: 17.59, latitude: 59.843},
+    {user_id: 6, rating: 5, time: "just now", longitude: 17.64, latitude: 59.865},
+]
 
 @Component({
     selector: 'app-map',
@@ -34,12 +46,11 @@ export class MapComponent implements AfterViewInit {
 
     private initMap(): void {
         this.map = L.map('map', {
-            center: [uppsalaCentrum.lat, uppsalaCentrum.long],
+            center: [centerCoords.lat, centerCoords.long],
             zoom: 12,
-            minZoom: 6,
-            maxZoom: 12,
+            minZoom: 2,
+            //maxZoom: 12,
         })
-
 
 
         const tiles = L.tileLayer('https://api.mapbox.com/styles/v1/albinantti/ckzh3jx4r009q14l8eb32614u/tiles/{z}/{x}/{y}?access_token=' + environment.token, {
@@ -48,8 +59,6 @@ export class MapComponent implements AfterViewInit {
 
 
         tiles.addTo(this.map);
-
-
     }
 
 
@@ -63,6 +72,8 @@ export class MapComponent implements AfterViewInit {
             popupAnchor: [0, -12],
         });
 
+
+
         this.selfMarker = L.marker([59.858, 17.639],
             { icon: userMapIcon }
         )
@@ -70,6 +81,9 @@ export class MapComponent implements AfterViewInit {
         interval(5000).subscribe(x => {
             this.getLocation();
         });
+
+        this.drawReports(reportList);
+
     }
 
 
@@ -97,9 +111,73 @@ export class MapComponent implements AfterViewInit {
         this.selfMarker.setLatLng([latitude, longitude])
     }
 
-    drawReports(reports: [Report]) {
-        console.log(reports[0].rating)
-        return;
+
+    private reportIcon0 = L.icon({
+        iconUrl: 'assets/images/reporticon0.svg',
+        iconSize: [20, 20],
+        iconAnchor: [0, 10],
+        popupAnchor: [8, -12],
+    });
+    private reportIcon1 = L.icon({
+        iconUrl: 'assets/images/reporticon1.svg',
+        iconSize: [20, 20],
+        iconAnchor: [0, 10],
+        popupAnchor: [8, -12],
+    });
+    private reportIcon2 = L.icon({
+        iconUrl: 'assets/images/reporticon2.svg',
+        iconSize: [20, 20],
+        iconAnchor: [0, 10],
+        popupAnchor: [8, -12],
+    });
+    private reportIcon3 = L.icon({
+        iconUrl: 'assets/images/reporticon3.svg',
+        iconSize: [20, 20],
+        iconAnchor: [0, 10],
+        popupAnchor: [8, -12],
+    });
+    private reportIcon4 = L.icon({
+        iconUrl: 'assets/images/reporticon4.svg',
+        iconSize: [20, 20],
+        iconAnchor: [0, 10],
+        popupAnchor: [8, -12],
+    });
+    private reportIcon5 = L.icon({
+        iconUrl: 'assets/images/reporticon5.svg',
+        iconSize: [20, 20],
+        iconAnchor: [0, 10],
+        popupAnchor: [8, -12],
+    });
+
+    selectIcon(number: number) {
+        switch(number){
+            case 0:
+                return this.reportIcon0;
+            case 1:
+                return this.reportIcon1;
+            case 2:
+                return this.reportIcon2;
+            case 3:
+                return this.reportIcon3;
+            case 4:
+                return this.reportIcon4;
+            case 5:
+                return this.reportIcon5;
+            default:
+                return this.reportIcon0;
+
+        }
+    }
+
+    drawReports(reports: Report[]) {
+        for (var index in reports) {
+            L.marker([reports[index]['latitude'], reports[index]['longitude']]
+            ).setIcon(
+                this.selectIcon(reports[index]['rating'])
+            ).bindPopup(
+                `${reports[index]['rating']}/5 reported ${reports[index]['time']}.`
+            ).addTo(this.map);
+        }
     }
 
 }
